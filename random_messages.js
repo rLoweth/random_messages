286bd6105c3dfada5a6860a3f6ecc3f7decef9ce
Ryan <ryanloweth29@gmail.com>
2020-11-09 11:09:24 -0800

    added yet another word to each array.

--- random_messages.js
+++ random_messages.js
@@ -1,19 +1,19 @@
 const adj = () => {
-    const words = ['greatest', 'smallest', 'fastest', 'rarest', 'loneliest', 'slimiest'];
+    const words = ['greatest', 'smallest', 'fastest', 'rarest', 'loneliest', 'slimiest', 'nicest'];
     let random = Math.floor(Math.random() * (words.length -1));
     let result = words[random];
     return result;
 };
 
 const noun = () => {
-    const words = ['rabbit', 'train', 'helicopter', 'dinosaur', 'whale', 'jewel', 'actor', 'strawberry', 'spoon', 'stranger', 'octopus'];
+    const words = ['rabbit', 'train', 'helicopter', 'dinosaur', 'whale', 'jewel', 'actor', 'strawberry', 'spoon', 'stranger', 'octopus', 'violin'];
     let random = Math.floor(Math.random() * (words.length -1));
     let result = words[random];
     return result;
 };
 
 const place = () => {
-    const words = ['China', 'the world', 'India', 'Wisconsin', 'the sea', 'outer space', 'reality', 'MIT', 'the circus'];
+    const words = ['China', 'the world', 'India', 'Wisconsin', 'the sea', 'outer space', 'reality', 'MIT', 'the circus', 'Antarctica'];
     let random = Math.floor(Math.random() * (words.length -1));
     let result = words[random];
     return result;
@@ -29,7 +29,7 @@ const always = () => {
 };
 
 const verb = () => {
-    const words = ['shines', 'speaks true', 'sinks', 'causes pandemoneum', 'cries', 'bounces back'];
+    const words = ['shines', 'speaks true', 'sinks', 'causes pandemoneum', 'cries', 'bounces back', 'explodes'];
     let random = Math.floor(Math.random() * (words.length -1));
     let result = words[random];
     return result;
